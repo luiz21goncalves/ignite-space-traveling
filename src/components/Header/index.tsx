96c3952c3b35bbb26229578c0d767ec6b246cd47
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-12-14 16:14:18 -0300

    add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.container}>
+      <nav className={styles.content}>
+        <Link href="/" passHref>
+          <a>
+            <Image
+              className={styles.image}
+              src="/assets/logo.svg"
+              alt="logo"
+              width={240}
+              height={26}
+            />
+          </a>
+        </Link>
+      </nav>
+    </header>
+  );
 }
